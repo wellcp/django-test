80d8f66a7c252457ec6c6771bda58f1f88664f39
Wellington C Prado <well.cp@hotmail.com>
2017-12-06 02:23:40 -0200

    First commit

---
+++ DjangoWebProject2/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\wellcp\source\repos\DjangoWebProject2\DjangoWebProject2\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
